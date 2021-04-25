a30b04ec0061b8067fa41f1bdc9447f89025689c
xiaoxiaoran <2425940130@qq.com>
2021-04-25 14:16:11 +0800

    实现求各位之和

--- xiaoxiaoran1/8.c
+++ xiaoxiaoran1/8.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    
-}
-
-    
+    int number, sum = 0;
+    printf("Please enter a number-->");
+    scanf("%d", &number);
+    while (number)
+    {
+        sum += (number % 10);
+        number /= 10;
+    }
+    printf("This is the sum of this-->%d", sum);
 }
