7f3189efd973e2eee2146ca8bd170a02def46882
xiaoxiaoran <2425940130@qq.com>
2021-04-29 13:34:21 +0800

    增加账号登录

--- xiaoxiaoran1/11.c
+++ xiaoxiaoran1/11.c
@@ -1,19 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
+int login(char account[]);
+void password(char x[]);
+//account-->xiao123 password-->xiao
 int main()
 {
+    int result = 1;
+    char xiao_account[10];
+    char xiao_password[10];
+    while (result)
+    {
+        result = login(xiao_account);
+        if (result == 1)
+            printf("\nAccount does not exist\nPlease re-enter-->");
+    }
+    password(xiao_password);
+}
+
+int login(char account[])
+{
+    char x_login[] = {"xiao123"};
+    printf("Please enter your account-->");
+    gets(account);
+    if (strcmp(strlwr(account), strlwr(x_login)) == 0)
+        return 0;
+    return 1;
+}
+
+void password(char password[])
+{
     int num = 3;
-    char password[10];
     char x_password[10] = {"xiao"};
-    printf("Please enter your password-->");
+    printf("\nPlease enter your password-->");
     while (1)
     {
-
         gets(password);
         if (strcmp(password, x_password) == 0)
         {
-            printf("xiuxiu");
+            printf("login successful");
             break;
         }
         else
