19fe851e5039806b025f6fc036f6ba912d310470
xiaoxiaoran <2425940130@qq.com>
2021-05-21 09:48:21 +0800

    字符指针

---
+++ xiaoxiaoran1/xiao3/24.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    char xiao[] = "xiaoxiaoran zui xiuxiuxiu";
+    char *x_xiao = "xiaoxiaoranxiuxiuxiu";
+    printf("%s--%c--%s", xiao,xiao[0],x_xiao);
+
+
+}
