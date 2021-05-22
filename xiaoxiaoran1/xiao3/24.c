32ec670f7fef4909419f81b853dc3a7ae4acd8e7
xiaoxiaoran <2425940130@qq.com>
2021-05-22 19:54:53 +0800

    日常无语

--- xiaoxiaoran1/xiao3/24.c
+++ xiaoxiaoran1/xiao3/24.c
@@ -1,10 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
     char xiao[] = "xiaoxiaoran zui xiuxiuxiu";
     char *x_xiao = "xiaoxiaoranxiuxiuxiu";
-    printf("%s--%c--%s", xiao,xiao[0],x_xiao);
-
-
+    printf("%s--%c--%s", xiao, xiao[0], x_xiao);
 }
