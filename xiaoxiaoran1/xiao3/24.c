501c866ebfdd5d18a540ffaa639466830cff6d4b
xiaoxiaoran <2425940130@qq.com>
2021-05-26 16:30:43 +0800

    秀秀指针

--- xiaoxiaoran1/xiao3/24.c
+++ xiaoxiaoran1/xiao3/24.c
@@ -25,11 +25,11 @@ void pointer_adjustment()
 }
 
 //指向函数的指针作函数参数
-int add(int x,int y)
+int add(int x, int y)
 {
     return x + y;
 }
-int Pointer_to_function_parameters(int x,int y,int (*p)(int,int))
+int Pointer_to_function_parameters(int x, int y, int (*p)(int, int))
 {
     int max(int, int);
     int add(int, int);
@@ -38,12 +38,53 @@ int Pointer_to_function_parameters(int x,int y,int (*p)(int,int))
     return xiao;
 }
 
+//返回指针
+float *Return_pointer(float (*pointer)[4], int n)
+{
+    float *pt;
+    pt = *(pointer + n);
+    return pt;
+}
 
+//指针数组
+void Pointer_array(char *name[], int n)
+{
+    int i = 0;
+    char *p;
+    p = name[0];
+    while (i < n)
+    {
+        p = *(name + i++);
+        printf("%s\n", p);
+    }
+}
+
+//多重指针
+void Multiple_pointer()
+{
+    // int a[2] = {1, 2};
+    // int *num[2] = {&a[0], &a[1]};
+    // int **P , i;
+    // p = num[0];  //报错 未定义p
+    // for (i = 0; i < 2; i++,p++)
+    //     printf("%d ", **p);
+}
 
 int main()
 {
     //pointer_adjustment();
+
     int a;
-    a=Pointer_to_function_parameters(1, 2, add);
-    printf("%d", a);
+
+    //a = Pointer_to_function_parameters(1, 2, max);  //2
+
+    // float score[][4] = {{1, 1, 1, 1}, {2, 2, 2, 2}, {3, 3, 3, 3}};
+    // float *p = Return_pointer(score, 0);
+    // for (int i = 0; i < 4;i++)
+    //     printf("%f ", *(p + i));
+
+    // char *name[] = {"aaaa", "ssss", "ddddd", "fffffff", "ggggg"};
+    // Pointer_array(name, 5);
+
+    Multiple_pointer();
 }
