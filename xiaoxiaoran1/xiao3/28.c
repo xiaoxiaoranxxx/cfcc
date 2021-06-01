5510c648092385e2001c21efc49605efa7b0935b
xiaoxiaoran <2425940130@qq.com>
2021-06-01 22:17:55 +0800

    结构体指针

--- xiaoxiaoran1/xiao3/28.c
+++ xiaoxiaoran1/xiao3/28.c
@@ -1,15 +1,44 @@
 #include <stdio.h>
+#include <string.h>
+
+struct student
+{
+    long num;
+    char name[20];
+    char sex;
+    float score;
+};
+
+struct student max(struct student stu[])
+{
+    int i, m = 0;
+    for (i = 0; i < 4; i++)
+        if (stu[i].name > stu[m].name)
+            m = i;
+    return stu[m];
+}
+
+void print(struct student sss)
+{
+    printf("%d", sss.num);
+}
+
 int main()
 {
-    char w[20], a[5][10] = {"abcdef", "ghijkl", "mnopq", "rstuv", "wxyz"};
-    int i, j;
-    for (i = 0; i < 5; i++)
-    {
-        j = 0;
-        while (a[i][j] != '\0')
-            j++;
-        w[i] = a[i][j - 2];
-    }
-    w[5] = '\0';
-    puts(w);
+    struct student stu_1;
+    struct student *p;
+    p = &stu_1;
+    stu_1.num = 12212;
+    strcpy(stu_1.name, "xiaoxiaoran");
+    stu_1.sex = 'n';
+
+    printf("%d,%d,%d\n", stu_1.num, (*p).num, p->num);
+
+    struct student xiao[4] = {{1, "1", '1', 1.1}, {2, "1", '1', 1.1}, {3, "1", '1', 1.1}, {4, "1", '1', 1.1}};
+    for (p = xiao; p < xiao + 4; p++)
+        printf("%d ", p->num);
+    printf("\n");
+
+    p = xiao;
+    print(max(p));
 }
