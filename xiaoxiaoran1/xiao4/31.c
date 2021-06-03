7e95dd51745f3426868209158cee728ef3b1e7e2
xiaoxiaoran <2425940130@qq.com>
2021-06-03 19:55:19 +0800

    共用体

--- xiaoxiaoran1/xiao4/31.c
+++ xiaoxiaoran1/xiao4/31.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
+
+union data
+{
+    int ii;
+    char cc;
+    float ff;
+} a1;
+
 int main()
 {
-    int i, t[][3] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    for (i = 0; i < 3; i++)
-        printf("%d ", t[2 - i][i]);
+    //共用体只能有一个成员,其他会覆盖
+    union data a2;
+
+    a2.ii = 100;
+    printf("%d %c %f\n", a2.ii, a2.cc, a2.ff);
+
+    a2.cc = 'g';
+    printf("%d %c %f\n", a2.ii, a2.cc, a2.ff);
+
+    a2.ff = 1.111;
+    printf("%d %c %f\n", a2.ii, a2.cc, a2.ff);
+
+    enum weekday
+    {
+        qqqqqqq,
+        wwwwwwwww,
+        eeeeee,
+        ree,
+        te,
+        y,
+        u
+    };
+
+    enum weekday xiao;
+
+    xiao = y;
+
+    
 }
