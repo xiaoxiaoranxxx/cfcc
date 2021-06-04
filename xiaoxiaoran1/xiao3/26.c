6a79d31183d70edfeb9bd749d44c9ae5e821afdf
xiaoxiaoran <2425940130@qq.com>
2021-06-04 21:08:27 +0800

    利用异或在不使用第三方变量交换

--- xiaoxiaoran1/xiao3/26.c
+++ xiaoxiaoran1/xiao3/26.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+
+void swap(int a, int b)
+{
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("a=%d,b=%d", a, b);
+    
+    /*
+    **0010 a = 2
+    **0011 b = 3
+    **0001 a = a^b = 1
+    **0010 b = a^b = 3
+    **0011 a = a^b = 2
+    */
+}
+
 int main()
 {
     int a = 25, b = 9, i;
@@ -17,10 +34,10 @@ int main()
                                                  //00000010011
                                                  //00000001001
 
-    printf("The ~%d is %d \n", a, ~a);           //计算a进行取反运算的值-->~
-                                                 //00000011010
-                                                 //11111100101
-                                                 
+    printf("The ~%d is %d \n", a, ~a); //计算a进行取反运算的值-->~
+                                       //00000011010
+                                       //11111100101
+
     printf("decimal\t\tshift left by\tresult\n");
     for (i = 1; i < 9; i++)
     {
@@ -33,4 +50,7 @@ int main()
         b = a >> i;                          //使a右移i位
         printf("%d\t\t%d\t\t%d\n", a, i, b); //输出当前右移结果
     }
+
+    printf("a=%d,b=%d\n", a, b); // a = 25,b = 0
+    swap(a, b);                  // a = 0, b = 25
 }
