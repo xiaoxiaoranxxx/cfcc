203d5a298ea5effbc683be2202b3d966a6a4afdb
肖萧然 <78064939+xiaoxiaoranxxx@users.noreply.github.com>
2021-07-14 19:44:38 +0800

    优化
    优化

--- xiaoxiaoran2/xiao2/30.cpp
+++ xiaoxiaoran2/xiao2/30.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
+#include <algorithm>
 #include <cstring>
 using namespace std;
-/*给出班里某门课程的成绩单，请你按成绩从高到低对成绩单排序输出，如果有相同分数则名字字典序小的在前。*/
+
 struct student
 {
+    //string name;
     char name[21];
     int score;
-};
+} s[21];
+
+bool cmp(student a, student b)
+{
+    if (a.score == b.score)
+        //if(a.name.compare(b.name) <= 0)
+        if (strcmp(a.name, b.name) <= 0)
+            return true;
+    return a.score > b.score;
+}
+
 int main()
 {
-    struct student s[21];
     int n;
     cin >> n;
     for (int i = 0; i < n; i++)
         cin >> s[i].name >> s[i].score;
-    for (int i = 0; i < n - 1; i++)
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (s[j].score < s[j + 1].score)
-                swap(s[j + 1], s[j]);
-            if (s[j].score == s[j + 1].score && strcmp(s[j].name, s[j + 1].name) > 0)
-                swap(s[j + 1], s[j]);
-        }
+
+    // for (int i = 0; i < n - 1; i++)
+    //     for (int j = 0; j < n - i - 1; j++)
+    //     {
+    //         if (s[j].score < s[j + 1].score)
+    //             swap(s[j + 1], s[j]);
+    //         if (s[j].score == s[j + 1].score && strcmp(s[j].name, s[j + 1].name) > 0)
+    //             swap(s[j + 1], s[j]);
+    //     }
+
+    sort(s, s + n, cmp);
     for (int i = 0; i < n; i++)
         cout << s[i].name << " " << s[i].score << endl;
     return 0;
@@ -33,3 +47,4 @@ Hanmeimei 90
 Joey 92
 Tim 28
 */
+
