62006e825ccde34854d47d831a9578f8b70dea14
xiaoxiaoran <2425940130@qq.com>
2021-09-11 14:28:18 +0800

    sequence_table

--- xiaoxiaoran3/xiao0/Sequence_table.cpp
+++ xiaoxiaoran3/xiao0/Sequence_table.cpp
@@ -1,7 +1,7 @@
 #define overflow -1
 #define ok 1
 #define error 0
-#define maxsize 6
+#define maxsize 16
 #define increment 10
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,9 +16,6 @@ typedef struct
     int listsize;
 } sqlist;
 
-//typedef struct 是为了使用这个结构体方便。
-//若用typedef，可以这样写：typedef struct node{}NODE; 。在申请变量时就可以这样写：NODE n;其实就相当于 NODE 是node 的别名。区别就在于使用时，是否可以省去struct这个关键字。
-
 status initlist(sqlist &L)
 {
     L.elem = (elemtype *)malloc(maxsize * sizeof(elemtype));
@@ -26,6 +23,47 @@ status initlist(sqlist &L)
         exit(overflow);
     L.length = 0;
     L.listsize = maxsize;
+    return ok;
+}
+
+status insertlist(sqlist &L, int i, elemtype e)
+{
+    if (i < 1 || i > L.length)
+        return error;
+    elemtype *p;
+    int j;
+    if (L.length >= L.listsize)
+    {
+        p = (elemtype *)realloc(L.elem, (L.listsize + increment) * sizeof(elemtype));
+        if (!p)
+            exit(overflow);
+        L.elem = p;
+        L.listsize += increment;
+    }
+    for (j = L.length - 1; j >= i - 1; --j)
+        L.elem[j + 1] = L.elem[j];
+    L.elem[j + 1] = e;
+    L.length++;
+    return ok;
+}
+
+void printlist(sqlist L)
+{
+    int i;
+    for (i = 0; i < L.length; i++)
+        printf("%d ", L.elem[i]);
+    printf("\n");
+}
+
+status dellist(sqlist &L, int i, elemtype &e)
+{
+    int j;
+    if (i < 1 || i > L.length)
+        return error;
+    e = L.elem[i - 1];
+    for (j = i; j < L.length; j++)
+        L.elem[j - 1] = L.elem[j];
+    --L.length;
     return ok;
 }
 
@@ -37,6 +75,24 @@ int main()
 
     if (!initlist(LL)) //初始化
         return error;
+
+    for (i = 0; i < 10; i++)
+        LL.elem[i] = i * 8;
+    LL.length += 10;
+
+    //printf("value index-->");  //插入
+    //scanf("%d %d", &x, &r);
+    x = 188, r = 3;
+    insertlist(LL, r, x);
+
+    printlist(LL);
+
+    //printf("index-->");  //删除
+    //scanf("%d", &r);
+    dellist(LL, r, x);
+
+    printlist(LL);
+
 
     printf("xiaoxiaoran");
 }
